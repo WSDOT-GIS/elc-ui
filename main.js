f87e57960eb7c697eec493073019dfb7dde3e8ba
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2015-03-10 16:14:05 -0700

    Added UI and demo page

---
+++ main.js
@@ -0,0 +1,12 @@
+﻿/*global define*/
+define(["dojo/text!./Templates/elc-ui.min.html"], function (templateHtml) {
+	function ElcUI(rootNode) {
+		var parser = new DOMParser();
+		var doc = parser.parseFromString(templateHtml, "text/html");
+		var uiDom = doc.body.querySelector(".elc-ui-root").cloneNode(true);
+		this.root = rootNode;
+		this.root.appendChild(uiDom);
+	}
+
+	return ElcUI;
+});
